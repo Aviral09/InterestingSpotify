8c54ad99edc6df4e91ce2b371eb8dfb7192e3e99
Aviral09 <aviralgoel2001@gmail.com>
2021-06-10 23:44:43 +0530

    Deployment

--- src/spotify.js
+++ src/spotify.js
@@ -2,28 +2,28 @@
 // documentation/web-playback-sdk
 
 export const authEndpoint = "https://accounts.spotify.com/authorize";
-const redirectUri = "https://spotify-clone-d8b45.web.app/";
-const clientId = "a98e4972a94041b7b5fce71d3b8a3c6c";
+const redirectUri = "https://interestingspotify-33c45.web.app/";
+const clientId = "26bd7f3b5d0440f1b900eb2bd45120ed";
 
 const scopes = [
-  "user-read-currently-playing",
-  "user-read-recently-played",
-  "user-read-playback-state",
-  "user-top-read",
-  "user-modify-playback-state",
+	"user-read-currently-playing",
+	"user-read-recently-played",
+	"user-read-playback-state",
+	"user-top-read",
+	"user-modify-playback-state",
 ];
 
 export const getTokenFromUrl = () => {
-  return window.location.hash
-    .substring(1)
-    .split("&")
-    .reduce((initial, item) => {
-      let parts = item.split("=");
-      initial[parts[0]] = decodeURIComponent(parts[1]);
-      return initial;
-    }, {});
+	return window.location.hash
+		.substring(1)
+		.split("&")
+		.reduce((initial, item) => {
+			let parts = item.split("=");
+			initial[parts[0]] = decodeURIComponent(parts[1]);
+			return initial;
+		}, {});
 };
 
 export const loginUrl = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join(
-  "%20"
+	"%20"
 )}&response_type=token&show_dialog=true`;
